ae01566f9c0aab7d769ba66d28713be417e6851d
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-12-06 05:24:58 +0000

    corrected 2 try

--- pribeiro/#00/Codeforces748Div3/exB.cpp
+++ pribeiro/#00/Codeforces748Div3/exB.cpp
@@ -4,13 +4,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int count_digit(int number) {
-   int count = 0;
-   while(number != 0) {
-      number = number / 10;
-      count++;
-   }
-   return count;
+const int INF = 10000;
+
+//how many digits i have to remove to match goal to digit strings
+int stepsDivisible(string goal, string digit){
+    int steps = 0;
+    while(true){
+        //cout << goal[0] << goal[1] << " " << digit[digit.size()-2] << digit[digit.size()-1] << " " << steps << endl;
+        if( digit.size() == 2){
+            if( goal == digit)
+                return steps;
+            else
+                return INF;
+        }
+        //if goal is the same as the last two digits of digit string
+        if (!goal.compare(digit.substr(digit.size()-2,digit.size()-1)) )
+            return steps;
+        
+	//digit.erase(0, min(digit.find_first_not_of('0'), digit.size()-1));
+        if( goal[1] != digit[digit.size()-1]){
+ 			digit = digit.substr(0,digit.size()-1);
+			steps++;
+			continue;   
+        }
+        if( goal[0] != digit[digit.size()-2]){
+			digit = digit.substr(0,digit.size()-2) + digit[digit.size()-1];
+			steps++;
+			continue;
+        }
+    }
 }
 
 int main()
@@ -21,60 +43,14 @@ int main()
     for(int i=0;i<t;i++){
 			long long n;
 			cin >>n;
-			
-			int steps = -1,nAux=0,nAux2=0; 
-			while(true){
-				if( n % 25 == 0){
-					steps++;
-					cout << steps << endl;
-					break;
-				}
-				int digitNum = count_digit(n);
-				string numberS = to_string(n);
-				steps++;
-				//if last-2 and last-1 are good remove last digit, else remove last-1 digit
-				if(digitNum > 2){
-					string lastDigits2 = string(1,numberS[digitNum-3])+numberS[digitNum-2];   
-					lastDigits2.erase(0, min(lastDigits2.find_first_not_of('0'), lastDigits2.size()-1));
-					nAux2 = stoi( lastDigits2 );
-				}
-				string lastDigits = string(1,numberS[digitNum-2])+numberS[digitNum-1];   
-				lastDigits.erase(0, min(lastDigits.find_first_not_of('0'), lastDigits.size()-1));
-				nAux = stoi( lastDigits );
-				//cout << n << " " << nAux << " " << nAux2 << endl;
-				if((numberS[digitNum-1] == '0' || numberS[digitNum-1] == '5' )){
-					if( (numberS[digitNum-2] == '2' || numberS[digitNum-2] == '5' || numberS[digitNum-2] == '7' || numberS[digitNum-2	] == '0') && (nAux % 25 == 0) ){
-							if( nAux2 % 25 == 0){
-								string newStr = numberS.substr(0,numberS.size()-1);
-								n = stoi(newStr);		
-								continue;
-							}
-							else{
-								string newStr = numberS.substr(0,numberS.size()-2) + numberS[digitNum-1];
-								n = stoi(newStr);
-								continue;
-							}
-					}
-					else{
-							if( nAux2 % 25 == 0){
-								string newStr = numberS.substr(0,numberS.size()-1);
-								n = stoi(newStr);		
-								continue;
-							}
-							else{
-								string newStr = numberS.substr(0,numberS.size()-2) + numberS[digitNum-1];
-								n = stoi(newStr);
-								continue;
-							}
-					}
-				}
-				else{
-					string newStr = numberS.substr(0,numberS.size()-1);
-					n = stoi(newStr);
-				}
-
-			}	
+			string nStr = to_string(n);
+		    //to be divisible by 25 last two digits must be 4 cases
+			int a = stepsDivisible("00",nStr);
+			int b = stepsDivisible("25",nStr);
+			int c = stepsDivisible("50",nStr);
+			int d = stepsDivisible("75",nStr);
+			int result = min(a,min(b,min(c,d)));
+            cout << result << endl;
 	}
     return 0;
 }
-
