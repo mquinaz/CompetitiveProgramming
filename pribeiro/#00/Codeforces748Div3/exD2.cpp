f90326818d9664ccb4018c4734e780d74559bc54
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-12-13 22:56:12 +0000

    wrong

---
+++ pribeiro/#00/Codeforces748Div3/exD2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include<bits/stdc++.h>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+
+int gcd(int a,int b){
+    if(b==0)return a;
+    else return gcd(b,a%b);
+}
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    for(int i=0;i<t;i++){
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for(int j=0;j<n;j++)
+		    cin >> a[j];
+
+		bool inf = true;
+		int minval = a[0];
+        
+        for(int j=1;j<n;j++){
+            if (a[j] != a[0]){
+                inf = false;
+                break;
+            }
+            minval = min(minval, a[j]);
+        }
+        if (inf)
+		{
+			cout << "-1\n";
+			continue;
+		}
+		sort(a.begin(), a.end());
+		
+		int ans = 0;
+		for (int j = 0; j < n; j++)
+			ans = gcd(ans, a[j] - minval);
+		cout << ans << '\n';
+    }
+    return 0;
+}
