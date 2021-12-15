c6e1c4b411f8a05df6a6a8fe737e43176b481996
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-12-15 23:50:33 +0000

    wrong answer

--- pribeiro/#00/Codeforces748Div3/exE.cpp
+++ pribeiro/#00/Codeforces748Div3/exE.cpp
@@ -36,22 +36,37 @@ int main()
 		}
         //printGraph(adj,n);
         while(k > 0){
+            vector<int> flagRemoveException;
             for (int v = 0; v < n; v++){
                 int nrVertex = 0;
                 for (auto x : adj[v])
                     nrVertex++;
-                if(n == 2){
+                /*if(n == 2){
                     adj[v].clear();
+                    //adj[adj[v][0]].clear();
                     k = 0;
                     break;
-                }
+                }*/
                 if(nrVertex == 1){
+                    int flagBreak = 0;
+                    for (auto x : flagRemoveException ){
+                        if( x == v){
+                            flagBreak=1;
+                            break;
+                        }
+                    }
+                    if(flagBreak)
+                        continue;
                     int count = 0;
                     //remove the opposite connection
                     for (auto x : adj[ adj[v][0] ]){
                         count++;
-                        if( x == v )
+                        if( x == v ){
                             adj[ adj[v][0] ].erase( (adj[ adj[v][0] ].begin() + count - 1) );
+                            //cout << " : " << adj[v][0] << " :" << v << endl;
+                            if(adj[v][0] > v)
+                                flagRemoveException.push_back(adj[v][0]);
+                        }
                     }
                     //remove vertex leaf
                     adj[v].pop_back();
@@ -63,8 +78,11 @@ int main()
         //printGraph(adj,n);
         int finalVertex = 0;
         for (int v = 0; v < n; v++)
-            for (auto x : adj[v])
+            for (auto x : adj[v]){
+                //cout << x << endl;
                 finalVertex++;
+                break;
+            }
         cout << finalVertex << endl;
     }
     return 0;
