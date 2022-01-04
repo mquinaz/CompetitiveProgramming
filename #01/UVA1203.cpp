771b98176a333bc247b7458439d3784163b9c9dd
mquinaz <36314718+mquinaz@users.noreply.github.com>
2022-01-04 18:19:45 +0000

    corrected

--- #01/UVA1203.cpp
+++ #01/UVA1203.cpp
@@ -8,26 +8,28 @@ int main()
     priority_queue<pi, vector<pi>, greater<pi> > pq;
     string x;
     int Q_num,P;
+    int DIC [5000] ;
+    
+    //priority_queue ordered by minimum Period with pair as ID(Q_num)
+    //DIC dictionary with the periods
     while(cin >> x){
         if(x == "#")
             break;
         cin >> Q_num >> P;
         pq.push(make_pair(P, Q_num));
+        DIC [Q_num] =  P;
     }
-    
     int K;
     cin >> K;
     
-    int totalInc = 0;
+    //here we pop the minimum period and add the same period, 200, 400, 600...300,600,900..
     while(K > 0){
-        pair<int, int> top = pq.top();
-        //explorar resto divisao inteira quando se repete o nr
-        if(top.first - totalInc <= 0){
-            pq.pop();
-            pq.push(make_pair(top.first, top.second));
-            cout << top.first << " " << top.second;
-        }
-        totalInc++;
+        pair<int, int> top = pq.top(); 
+        pq.pop();
+        top.first += DIC[top.second];
+        pq.push(make_pair(top.first, top.second));
+        cout << top.second << endl;
+        K--;
     }
     return 0;
 }
