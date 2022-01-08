b86f8c6363880734f75db26d628a0f3cbe72a8b9
mquinaz <36314718+mquinaz@users.noreply.github.com>
2022-01-08 17:17:16 +0000

    Initial performance on contest

---
+++ pribeiro/atCoder234/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int f(int x){
+	return x*x + 2*x + 3;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	
+	int res = f( f( f(t) + t) +f(f(t)) );
+	cout << res << endl;
+	return 0;
+}
