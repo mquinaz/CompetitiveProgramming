802a53e6b166dd86c7f609bb6c75ef585da1638c
mquinaz <36314718+mquinaz@users.noreply.github.com>
2022-02-09 15:33:43 +0000

    correct using solution

--- pribeiro/#01/UVA11572.cpp
+++ pribeiro/#01/UVA11572.cpp
@@ -1,62 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-//https://www.redgreencode.com/uva-11572-unique-snowflakes/
 
 int main()
 {
     int t;
     cin >> t;
-    for(int i=0;i<t;i++){
+    for(int i=0;i<t;i++)
+    {
         int n;
         cin >> n;
         
-        int max_seq = 0;
-        unordered_map<long long,int> snowflakePosition;
-        pair<int, int> segmentsByStart[n];
-        pair<int, int> segmentsByEnd[n];
-        for(int j=0;j<n;j++){
+        long long mx = 0;
+        long long u = 0;
+        unordered_map<int,int> m;
+        for(long long j=1;j<=n;j++)
+        {
             long long snow;
             cin >> snow;
             
-            int previousPos = -1;
-            for (auto itr = snowflakePosition.find(snow); itr != snowflakePosition.end(); itr++)
-                if(itr->second >= 0)
-                    previousPos = itr->second;
-                 
-            cout << previousPos << endl;
-            snowflakePosition.insert({snow,j});
-            if( previousPos < 0)
-                continue;
-            
-            else
+            int &r = m[snow];
+            if( r > u )
             {
-                int currentPos = j;
-                segmentsByStart[previousPos] = make_pair(previousPos,currentPos);
-                segmentsByEnd[currentPos] = make_pair(previousPos,currentPos);
-            }
+				mx = max(mx,j-u-1);
+				u = r;
+			}
+			r = j;
         }
-        
-        for(int p=0;p<n;p++)
-        {
-            unordered_map<long long,int>::iterator itr;
-            pair<int, int> s1 = segmentsByStart[p];
-            if(s1.second == -1)
-            {
-               pair<int, int> auxSegment = make_pair(p,n);
-            }
-            else
-                s1 = make_pair(-1,-1);
-            
-            int len = s1.second - s1.first;
-        
-            pair<int, int> s2 = segmentsByEnd[p];
-            if(s2.first >= s1.first && s2.second <= s1.second)
-                len = s1.first - s2.second;
-
-            if(len > max_seq)
-                max_seq = len;
-        }
-        cout << max_seq << endl;
+        mx = max(mx,n-u);
+		cout << mx << endl;
     }
 	
 	return 0;
