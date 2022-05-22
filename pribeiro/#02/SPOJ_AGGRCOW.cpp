a4b9e5c66fb7fa7f7d4df2d71de6cbef93f78ab7
mquinaz <36314718+mquinaz@users.noreply.github.com>
2022-05-22 19:19:24 +0100

    correct with algorithm view

--- pribeiro/#02/SPOJ_AGGRCOW.cpp
+++ pribeiro/#02/SPOJ_AGGRCOW.cpp
@@ -1,24 +1,60 @@
-#include <iostream>
-#include<math.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+bool isFeasible(long long mid, vector<long long> arr, int k)
+{
+    long long pos = arr[0];
+    int elements = 1;
+ 
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i] - pos >= mid)
+        {
+            pos = arr[i];
+            elements++;
+ 
+            if (elements == k)
+            {
+                return true;
+            }
+        }
+    }
+    return 0;
+}
 int main()
 {
     int t;
+    cin >> t;
     while(t--)
     {
-        int N,C;
-        cin >> N >> C;
+        int n,c;
+        cin >> n >> c;
         
-        vector<int> ord;
-        for(int i=0;i<N;i++)
+        vector<long long> numList;
+        numList.clear();
+        for(int i=0;i<n;i++)
         {
             long long x;
             cin >> x;
-            ord.push_back(x);
+            numList.push_back(x);
         }
-        sort(ord.begin(),ord.end());
-        int middle = (ord[ord.size()] - ord[0]) / 2;
+        sort(numList.begin(),numList.end());
+        
+        long long res=-1,left=1,right=numList[numList.size()-1];
+        while (left < right)
+        {
+            long long mid = (left + right) / 2;
+            if (isFeasible(mid, numList, c))
+            {
+                res = max(res, mid);
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        cout << res << endl;
     }
 
     return 0;
